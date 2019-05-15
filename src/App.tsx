c081d4d64773dc19489d4da47f6b4b2fde1b1e62
YBCoding <yb.coding@gmail.com>
2019-05-16 00:45:57 +0200

    Add react-leaflet and initial display

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import {LatLngTuple} from "leaflet";
+import 'leaflet/dist/leaflet.css'
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const position: LatLngTuple = [48.8534, 2.3488];
+    const zoom = 13;
+    let style = {width: '100%', height: '600px'};
+    return (
+        <Map center={position} zoom={zoom} style={style}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            />
+            <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br/> Easily customizable.
+                </Popup>
+            </Marker>
+        </Map>
+    );
+};
 
 export default App;
